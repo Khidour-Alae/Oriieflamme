44eee00ef8e486f339db31e70e093866c3007cfa
Hugo PICARD <hugo.picard@pc228-05.pedago.ensiie.fr>
2022-03-22 16:41:17 +0100

    Task B.6 Modified the function hasTheDeckBeenReshuffled

--- src/faction.c
+++ src/faction.c
@@ -3,17 +3,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define SIZE_NAME
+
 struct impl_faction{
     char f_name[SIZE_NAME];
     int f_ddrsPoints;
     hand f_hand;
     deck f_deck;
     int hasBeenReshuffled; // 1 if the deck has been reshuffled, 0 if not
-}
-
+};
 
 int hasTheDeckBeenShuffled(faction faction){
-
+    return faction->hasBeenReshuffled; 
 };
 
 void reshuffleDeck(faction *faction){
