b0fd0f2379175656de38c32598359a9a44730d1e
Hugo PICARD <hugo.picard@pc228-05.pedago.ensiie.fr>
2022-03-22 16:28:17 +0100

    Task B.6 Added functions

--- src/faction.c
+++ src/faction.c
@@ -1,3 +1,24 @@
 #include "../headers/faction.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+
+int hasTheDeckBeenShuffled(faction faction){
+
+};
+
+void reshuffleDeck(faction *faction){
+
+};
+
+void discardHand(faction *faction){
+
+};
+
+void shuffleDeck(faction *faction){
+
+};
+
+void drawCard(faction *faction){
+
+};
