4460dd8a1e2303c1aa339bb2ea89dabb9593323a
Alae KHIDOUR <alae.khidour@pc228-06.pedago.ensiie.fr>
2022-03-22 16:22:13 +0100

    Task B.5: upgrade struct card et create struct faction

--- src/faction.c
+++ src/faction.c
@@ -1,3 +1,13 @@
 #include "../headers/faction.h"
+#include "../headers/structure.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+
+struct impl_faction{
+    char f_name[SIZE_NAME];
+    int f_ddrsPoints;
+    hand f_hand;
+    deck f_deck;
+    int hasBeenReshuffled; // 1 if the deck has been reshuffled, 0 if not
+}
