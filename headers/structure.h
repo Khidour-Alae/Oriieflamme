8d9a37bf681e292ef62194fd0947c391a7f4dd80
Damien LE VASSEUR <damien.levasseur@pc228-02.pedago.ensiie.fr>
2022-03-22 16:13:52 +0100

    Task B.3 added usefull types for cards.h board.h faction.h

--- headers/structure.h
+++ headers/structure.h
@@ -1,4 +1,21 @@
 #ifndef _STRUCTURE_H  
 #define _STRUCTURE_H
 
+#include "cards.h"
+#include "faction.h"
+
+
+
+
+
+typedef card deck[NB_CARDS_IN_DECK];
+typedef card hand[NB_CARDS_IN_HAND];
+
+typedef struct coupleCardFaction
+{
+    card c;
+    faction f;
+} coupleCardFaction;
+typedef coupleCardFaction board2D[SIZE_2DBOARD]; 
+
 #endif
