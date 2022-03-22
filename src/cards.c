ca01e448f58923190ae758863d763f6872d3737d
Alae KHIDOUR <alae.khidour@pc228-06.pedago.ensiie.fr>
2022-03-22 15:02:34 +0100

    Task B.5: struct impl_card

--- src/cards.c
+++ src/cards.c
@@ -1,3 +1,22 @@
 #include "../headers/cards.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+#define SIZE_NAME 150
+#define EFFECT_DESCRIPTION 1000
+
+typedef enum cardName cardName;
+enum cardName {
+    Julien_Forest,
+    Alae_Khigour
+}
+
+
+struct impl_card{
+    char name[SIZE_NAME]; 
+    cardName cnale;
+    char effectDescription[EFFECT_DESCRIPTION];
+    int numberOfOccurrence;
+    int cardStatus; // 0 if Face Down, 1 if Face Up
+}
+
