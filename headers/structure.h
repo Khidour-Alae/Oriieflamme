9be2f1678437f3414e79757292bd3bb5af9e5de3
damien <damien.levasseur>
2022-03-24 21:40:44 +0100

    Task B.3 deck and hand structure with basic functions

--- headers/structure.h
+++ headers/structure.h
@@ -8,11 +8,38 @@
 #define NB_CARDS_IN_HAND 8
 #define SIZE_2DBOARD 1089
 
+//deck structure
+typedef struct {
+    card *c;
+    int top;
+} deck;
+
+void init_deck(deck *d);
+int isEmpty_deck(deck d);
+void push_deck(card c, deck *d);
+card pop_deck(deck *d);
+void shuffle_deck(deck *d);
+void reset_deck(deck *d);
+void delete_deck(deck *d);
+//void print_deck(deck *d);
 
 
-typedef card deck[NB_CARDS_IN_DECK];
-typedef card hand[NB_CARDS_IN_HAND];
+//hand structure
+typedef struct {
+    card *c;
+    int top;
+} hand;
 
+void init_hand(hand *h);
+int isEmpty_hand(hand h);
+void push_hand(card c, hand *h);
+card pop_hand(hand *h);
+void reset_hand(hand *h);
+void delete_hand(hand *h);
+//void print_hand(hand *h);
+
+
+//board structure
 typedef struct coupleCardFaction
 {
     card c;
--- src/structure.c
+++ src/structure.c
@@ -1,1 +1,88 @@
 #include "../headers/structure.h"
+#include <stdlib.h>
+#include <time.h>
+
+//deck structure
+void init_deck(deck *d) {
+    d->c = malloc(sizeof(card)*NB_CARDS_IN_DECK);
+    d->top = -1;
+}
+
+int isEmpty_deck(deck d) {
+    return d.top == -1;
+}
+
+void push_deck(card c, deck *d) {
+    if (d->top == NB_CARDS_IN_DECK - 1)
+    {
+        //raise error
+    }
+    else
+    {
+        d->top += 1;
+        d->c[d->top] = c;        
+    }
+}
+
+card pop_deck(deck *d) {
+    d->top -= 1;
+    return d->c[d->top + 1];
+}
+
+//using Fisher–Yates shuffle algorithm
+void shuffle_deck(deck *d) {
+    srand(time(NULL));
+    int j;
+    for (int i = 0; i < d->top - 2; i++)
+    {
+        j = rand()%((d->top)-i) + i;
+        card tmp = d->c[i];
+        d->c[i] = d->c[j];
+        d->c[j] = tmp;
+    }
+}
+
+void reset_deck(deck *d) {
+    free(d->c);
+    init_deck(d);
+}
+
+void delete_deck(deck *d) {
+    free(d->c);
+}
+
+
+
+//hand structure
+void init_hand(hand *h) {
+    h->c = malloc(sizeof(card)*NB_CARDS_IN_HAND);
+    h->top = -1;
+}
+int isEmpty_hand(hand h) {
+    return h.top == -1;
+}
+
+void push_hand(card c, hand *h) {
+        if (h->top == NB_CARDS_IN_HAND - 1)
+    {
+        //raise error
+    }
+    else
+    {
+        h->top += 1;
+        h->c[h->top] = c;        
+    }
+}
+
+card pop_hand(hand *h) {
+    h->top -= 1;
+    return h->c[h->top + 1];
+}
+
+void reset_hand(hand *h) {
+    free(h->c);
+    init_hand(h);
+}
+void delete_hand(hand *h) {
+    free(h->c);
+}
