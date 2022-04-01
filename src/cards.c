e7eb96aaf4e89d34a61a777a895212a2247c58cc
Hugo PICARD <hugo.picard@pc228-05.pedago.ensiie.fr>
2022-04-01 15:37:00 +0200

    Task B.4 Added the name of th e different cards

--- src/cards.c
+++ src/cards.c
@@ -7,8 +7,38 @@
 
 typedef enum cardName cardName;
 enum cardName {
+    FISE,
+    FISA,
+    FC,
+    EcologIIE,
+    lIIEns,
+    Soiree_sans_alcool,
+    Alcool,
+    Cafe,
+    The,
+    Ecocup,
+    Reprographie,
+    Iolation_du_batiment,
+    Parcours_sobriete_numerique,
+    Heures_supplementaires,
+    Kahina_Bouchama,
+    Kevin_Goilard,
+    Massinissa_Merabet,
+    Vitera_Y,
+    Jonas_Senizergues,
+    Fetia_Bannour,
+    Catherine_Dubois,
+    Anne_Laure_Ligozat,
+    Guillaume_Burel,
+    Christophe_Mouilleron,
+    Thomas_Lim,
     Julien_Forest,
-    Alae_Khigour
+    Dimitri_Watel,
+    Djibril_Aurélien_Dembele_Cabot,
+    Eric_Lejeune,
+    Lucienne_Pacave,
+    Katrin_Salhab,
+    Laurent_Prevel
 };
 
 
